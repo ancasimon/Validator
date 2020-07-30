14ff09ef5d8824e55f542404868cbeb8b3d8d031
Anca Simon <ancarsimon@gmail.com>
2020-07-29 19:12:40 -0500

    Add project files.

---
+++ Validator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Validate a pin:
+            Console.WriteLine("Please specify a pin.");
+            string userPin = Console.ReadLine();
+            bool isValidPin = false;
+            //int n; 
+            bool isNumericPin = int.TryParse(userPin, out _);
+            //Console.WriteLine($"isNumericPin bool value: {isNumericPin}");
+
+            if (userPin.Length >= 4 && userPin.Length <= 8 && isNumericPin == true)
+            {
+                isValidPin = true;
+                Console.WriteLine("Pin is valid!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, it's not a valid pin.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
