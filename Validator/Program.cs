709b8081db266ed47fb3da8bdb2ed8b5d6ee6560
Anca Simon <ancarsimon@gmail.com>
2020-07-29 20:21:54 -0500

    Added validation for a phone number (Part 2 in exercise).

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 
 namespace Validator
 {
@@ -6,24 +7,90 @@ namespace Validator
     {
         static void Main(string[] args)
         {
-            //Validate a pin:
+            //PART 1: Validate a pin:
             Console.WriteLine("Please specify a pin.");
             string userPin = Console.ReadLine();
-            bool isValidPin = false;
+            //bool isValidPin = false;
             //int n; 
             bool isNumericPin = int.TryParse(userPin, out _);
             //Console.WriteLine($"isNumericPin bool value: {isNumericPin}");
 
-            if (userPin.Length >= 4 && userPin.Length <= 8 && isNumericPin == true)
+            if (string.IsNullOrEmpty(userPin))
             {
-                isValidPin = true;
-                Console.WriteLine("Pin is valid!");
+                Console.WriteLine("You must enter a pin first!");
+            } 
+            else
+            {
+                if (userPin.Length >= 4 && userPin.Length <= 8 && isNumericPin == true)
+                {
+                    //isValidPin = true;
+                    Console.WriteLine("Pin is valid!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, it's not a valid pin.");
+                }
+            }
+            
+
+
+
+            //PART 2: Validate phone number:
+            Console.WriteLine("Please enter a phone number.");
+            string userPhoneNumber = Console.ReadLine();
+
+            //The next step allows the valid phone number characters in (space, -, () ), but it replaces them with no space and declares a new variable to store the new string created as a result of the Replace method.
+            string trimmedPhoneNumber = userPhoneNumber.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "");
+            Console.WriteLine(trimmedPhoneNumber);
+
+            //Then we check that the trimmed string includes only numeric characters:
+            bool isNumericOnly = Int64.TryParse(trimmedPhoneNumber, out long _);
+            Console.WriteLine($"numeric only? {isNumericOnly}");
+
+            //Next we will count the number of numeric characters in the phone number:
+            int digitCount = 0;
+            foreach(char c in trimmedPhoneNumber)
+            {
+                if (Char.IsDigit(c))
+                    digitCount++;
+            }
+            Console.WriteLine("digit count: "+digitCount);
+
+            //Last we make sure the first 3 characters are not 555:
+            string userPhoneFirstThree;
+            if (trimmedPhoneNumber.Length > 2)
+            {
+                userPhoneFirstThree = trimmedPhoneNumber.Substring(0, 3);
+                //Console.WriteLine($"first 3 characters are: {userPhoneFirstThree}");
+            } 
+            else
+            {
+                userPhoneFirstThree = "";
+            }
+
+            if (string.IsNullOrEmpty(userPhoneNumber))
+            {
+                Console.WriteLine("You must enter a phone number!");
             }
             else
             {
-                Console.WriteLine("Sorry, it's not a valid pin.");
+                if (isNumericOnly == true && digitCount == 10 && userPhoneFirstThree != "555")
+                {
+                    Console.WriteLine("Valid phone number!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, not a valid phone number! Try again!");
+                }
             }
+
+
+
+
+
+
             Console.ReadKey();
+
         }
     }
 }
