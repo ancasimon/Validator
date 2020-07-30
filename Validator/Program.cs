7f772e67b1216ce145972be88e3bacaf66b22c46
Anca Simon <ancarsimon@gmail.com>
2020-07-30 00:49:45 -0500

    WIP on Sponge Bob Square Pants.

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -20,7 +20,7 @@ namespace Validator
             if (string.IsNullOrEmpty(userPin))
             {
                 Console.WriteLine("You must enter a pin first!");
-            } 
+            }
             else
             {
                 if (userPin.Length >= 4 && userPin.Length <= 8 && isNumericPin == true)
@@ -33,7 +33,7 @@ namespace Validator
                     Console.WriteLine("Sorry, it's not a valid pin.");
                 }
             }
-            
+
 
 
 
@@ -51,7 +51,7 @@ namespace Validator
 
             //Next we will count the number of numeric characters in the phone number:
             int digitCount = 0;
-            foreach(char c in trimmedPhoneNumber)
+            foreach (char c in trimmedPhoneNumber)
             {
                 if (Char.IsDigit(c))
                     digitCount++;
@@ -64,7 +64,7 @@ namespace Validator
             {
                 userPhoneFirstThree = trimmedPhoneNumber.Substring(0, 3);
                 //Console.WriteLine($"first 3 characters are: {userPhoneFirstThree}");
-            } 
+            }
             else
             {
                 userPhoneFirstThree = "";
@@ -103,7 +103,7 @@ namespace Validator
             else //Feels I should have en else here because otherwise, I get an error if the domainName substring below has to start an a nonexistent number ....
             {
                 userName = userEmail.Substring(0, indexPositionOfAt);
-                domainName = userEmail.Substring(indexPositionOfAt+1); //Added plus 1 so that I don't include the @ in the domain name - and thus can make sure it doesn't include extra @!!!
+                domainName = userEmail.Substring(indexPositionOfAt + 1); //Added plus 1 so that I don't include the @ in the domain name - and thus can make sure it doesn't include extra @!!!
 
                 int indexPositionOfDot = domainName.IndexOf('.');
 
@@ -130,21 +130,113 @@ namespace Validator
                             userName.Length > 0 &&
                             domainName.Length > 0 &&
                             dotComString.Length > 0)
-                        {
-                            Console.WriteLine("You got it! Valid!!!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sorry - invalid email address. Please try again!");
-                        }
+                    {
+                        Console.WriteLine("You got it! Valid!!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry - invalid email address. Please try again!");
+                    }
 
                 }
 
             }
 
 
-            //PART 4 - SpongeBob SquarePants???
+            //PART 4 - Validate sentence case and capitalization. 
+            Console.WriteLine("Try me! What's the craziest way in which you can randomly capitalize characters in a phrase? Can you beat: mOcKiNg SoMeOnE lIkE sPoNgEbOb??");
 
+            //static bool checkGrammar(char[] str)
+            //{
+                //1 - Start with a string:
+                string userPhrase = Console.ReadLine();
+                
+                if (userPhrase.Length == 0)
+                {
+                Console.WriteLine("You must type a sentence!");
+                }
+
+                //2 - Calculate the length of the user's input:
+                int userPhraseLength = userPhrase.Length;
+
+                //3 - Check if the first character is a capital letter:
+                bool isFirstLetterCapital = false;
+                if (userPhrase[0] >= 'A' || userPhrase[0] <= 'Z')
+                {
+                    isFirstLetterCapital = true;
+                }
+
+                //4 - Check if the last character is a period:
+                bool isLastCharPeriod = false;
+                if (userPhrase[userPhraseLength - 1] == '.')
+                {
+                    isLastCharPeriod = true;
+                }
+
+                //5 - Set states for each character in the user input so that you can compare them and check for the following rules:
+                //Two continuous spaces are not allowed. 
+                //Two continuous upper case characters are not allowed. 
+                //Lowercase must follow an uppercase character.
+                //There must be spaces between words - HOW DO WE CHECK THAT??
+
+                int prev_state = 0, curr_state = 0;
+                bool checkSentence = false;
+
+                //Capture the index of the character in the string. 
+                int indexPosition = 1;
+
+                //Loop over the string and set the state for each character:
+                while (indexPosition <= userPhrase.Length)
+                {
+
+                    //If the current caharacter is A-Z, then set the current state as 0:
+                    if (userPhrase[indexPosition] >= 'A' && userPhrase[indexPosition] <= 'Z')
+                        curr_state = 0;
+
+                    //If current character is a space, then set the current state as 1:
+                    else if (userPhrase[indexPosition] == ' ')
+                        curr_state = 1;
+
+                    //If current character is [a-z], then set current state as 2:
+                    else if (userPhrase[indexPosition] >= 'a' && userPhrase[indexPosition] <= 'z')
+                        curr_state = 2;
+
+                    //If current character is a period, then set current state as 3:
+                    else if (userPhrase[indexPosition] == '.')
+                        curr_state = 3;
+
+                    //6 - Compare states to validate that the rules outlined above under step 5 are met:
+                    Console.WriteLine($"previous and current states for each character: {prev_state} and {curr_state}");
+
+                    //If a character is anything but a lower-case character, then it cannot follow a capital letter - so you wouldn't have a capital letter before a space:
+                    if (prev_state == curr_state && curr_state != 2)
+                    {
+                        checkSentence = false;
+                    }
+
+                    //If 
+                    if (prev_state == 2 && curr_state == 0)
+                    {
+                        checkSentence = false;
+                    }
+
+                    //
+                    if (curr_state == 3 && prev_state != 1)
+                        checkSentence = true;
+                        //return (indexPosition + 1 == userPhrase.Length);
+
+                    indexPosition++;
+
+                    //Set previous state as current state before foing over to the next character:
+                    prev_state = curr_state;
+
+                }
+                checkSentence = false;
+                //return false;
+
+            //}
+
+            //checkGrammar(userPhrase.ToCharArray());
 
 
             //PART 5 - Power Ranger name validation:
@@ -186,10 +278,10 @@ namespace Validator
             bool isPalindrome = userString.Equals(reversedUserString, StringComparison.OrdinalIgnoreCase);
             if (userString.Length > 0 && isPalindrome == true)
             {
-                Console.WriteLine("Yes! " + userString + " is a palindrome!!");
+                Console.WriteLine("Yes! " + "\"" + userString + "\"" + " is a palindrome!!");
             } else
             {
-                Console.WriteLine("Bummer: " + userString + " is not a palindrome ...");
+                Console.WriteLine("Bummer: " + "\"" + userString + "\"" + " is not a palindrome ...");
             }
 
 
